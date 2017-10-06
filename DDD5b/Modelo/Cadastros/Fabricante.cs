6b419c3730e4b4ac8b4215ff12e2d4b6fed71bf3
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-10-06 17:03:31 -0300

    Criação das classes Categoria, Fabricante e Produto

---
+++ DDD5b/Modelo/Cadastros/Fabricante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Cadastros
+{
+    public class Fabricante
+    {
+        public long? FabricanteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ DDD5b/Modelo/Cadastros/Produto.cs
@@ -0,0 +1,24 @@
+﻿using Modelo.Tabelas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Cadastros
+{
+    public class Produto
+    {
+        public long? ProdutoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public long? CategoriaId { get; set; }
+
+        public long? FabricanteId { get; set; }
+
+        public virtual Categoria Categoria {get; set; }
+
+        public virtual Fabricante Fabricante { get; set; }
+    }
+}
